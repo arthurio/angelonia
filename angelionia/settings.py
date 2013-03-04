866c5c235db15ba1924da3ea38b06228d333d883
Arthur Rio <arthurio8@hotmail.com>
2013-03-03 17:26:57 -0800

    procfile

--- angelionia/settings.py
+++ angelionia/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gunicorn',
     'chalet',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
