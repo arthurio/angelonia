a9975367c202aba344adf3dc7b533da516816cb4
Arthur Rio <arthurio8@hotmail.com>
2013-03-03 20:11:32 -0800

    change settings

---
+++ angelionia/settings/prod.py
@@ -0,0 +1,17 @@
+from angelia.settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+STATICFILES_DIRS = (
+        'angelionia/static/',
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
