93dbc963aac790bc5e604d2c63d0fd2b655a3161
Arthur Rio <arthurio8@hotmail.com>
2013-03-04 00:57:12 -0800

    get it to work

--- angelionia/settings/prod.py
+++ angelionia/settings/prod.py
@@ -1,8 +1,5 @@
 from angelionia.settings import *
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
