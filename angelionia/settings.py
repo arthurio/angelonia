408ed0baa6debb861a2f9a41caa8aa8ae965125e
Arthur Rio <arthurio8@hotmail.com>
2013-03-03 18:33:10 -0800

    settings

--- angelionia/settings.py
+++ angelionia/settings.py
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'gunicorn',
     'chalet',
+    'chalet.wsgi',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
