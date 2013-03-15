d37c58db001128d41c16c2fd9ef06e18350376ff
Arthur Rio <arthurio8@hotmail.com>
2013-03-15 00:52:37 -0700

    put back the email settings in __init__

--- angelonia/settings/__init__.py
+++ angelonia/settings/__init__.py
@@ -157,4 +157,8 @@ LOGGING = {
     }
 }
 
-
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'chalet.angelonia@gmail.com'
+EMAIL_HOST_PASSWORD = 'aigremont'
+EMAIL_PORT = 587
--- angelonia/settings/prod.py
+++ angelonia/settings/prod.py
@@ -6,9 +6,3 @@ DATABASES['default'] =  dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'chalet.angelonia@gmail.com'
-EMAIL_HOST_PASSWORD = 'aigremont'
-EMAIL_PORT = 587
